6a5ee92b62c77d7b3344bd15fb77e7b22e0c222a
Squidyy <lordsquid@gmail.com>
2024-12-19 06:58:20 -0500

    Feat: adding ability to move lines up 1 / down 1 in nvim

--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -1,3 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- configure move line up and down ( J, K in visual mode)
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
