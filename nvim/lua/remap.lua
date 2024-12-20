4d8b8ea74e5e34a1b81c99ffaafde04f03c6237b
Craig <lordsquid@gmail.com>
2024-12-19 22:58:28 -0500

    Feat: adding bufferline and lualine (commented out in filename)

--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -4,3 +4,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 -- configure move line up and down ( J, K in visual mode)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- toggle numberline and relative
+--vim.keymap.set("n", "<leader>n", ":set nornu<CR>" )
+--vim.keymap.set("n", "leader>N", ":set rnu<CR>")
